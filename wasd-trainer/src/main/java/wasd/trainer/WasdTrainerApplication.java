0a94a9bd7aa56f7a084730f0c8da41c43bf4ad6c
King-Keith1 <keith4321ben@gmail.com>
2025-11-30 16:55:38 +0200

    commit

---
+++ wasd-trainer/src/main/java/wasd/trainer/WasdTrainerApplication.java
@@ -0,0 +1,13 @@
+package wasd.trainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasdTrainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WasdTrainerApplication.class, args);
+    }
+
+}
---
+++ wasd-trainer/src/test/java/wasd/trainer/WasdTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package wasd.trainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasdTrainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
