7476a51800fc63a9430247863a10cb5be88d23d7
King-Keith1 <keith4321ben@gmail.com>
2025-12-04 16:23:21 +0200

    commit

--- tut-api/src/main/java/tutapi/model/Tutorial.java
+++ tut-api/src/main/java/tutapi/model/Tutorial.java
@@ -1,14 +1,23 @@
 package tutapi.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Tutorial model")
 public class Tutorial {
 
+    @Schema(description = "ID of the tutorial", example = "1")
     private Long id;
+
+    @Schema(description = "Title of the tutorial", example = "Spring Boot + Swagger")
     private String title;
+
+    @Schema(description = "Description of the tutorial", example = "How to integrate Swagger with Spring Boot")
     private String description;
+
+    @Schema(description = "Published flag", example = "true")
     private boolean published;
 
-    public Tutorial() {
-    }
+    public Tutorial() {}
 
     public Tutorial(Long id, String title, String description, boolean published) {
         this.id = id;
@@ -17,35 +26,16 @@ public class Tutorial {
         this.published = published;
     }
 
-    public Long getId() {
-        return id;
-    }
+    // getters & setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isPublished() {
-        return published;
-    }
-
-    public void setPublished(boolean published) {
-        this.published = published;
-    }
+    public boolean isPublished() { return published; }
+    public void setPublished(boolean published) { this.published = published; }
 }
