2bf26fe69803e857678aa66a4ebad54a64eaa9d4
King-Keith1 <keith4321ben@gmail.com>
2025-12-04 16:08:54 +0200

    commit

---
+++ tut-api/src/main/java/tutapi/TutApiApplication.java
@@ -0,0 +1,13 @@
+package tutapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutApiApplication.class, args);
+    }
+
+}
---
+++ tut-api/src/test/java/tutapi/TutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tutapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
